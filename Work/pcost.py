129bf50537eec8d3bf21afa419c1cd1c5f63cb0c
Katie Brown <kcb515@mun.ca>
2020-07-13 15:41:44 -0230

    1.27'

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+total = 0.0
+
+with open('portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        nshares= int(row[1])
+        price = float(row[2])
+        total += nshares * price
+
+print('Total', total)
+    
