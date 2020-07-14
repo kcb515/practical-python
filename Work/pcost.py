575f165774cdbbfa06d69f8459c98a0e5bb74e43
Katie Brown <kcb515@mun.ca>
2020-07-13 21:33:46 -0230

    Done exercises

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
-total = 0.0
+def portfolio_cost(filename):
 
-with open('portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        nshares= int(row[1])
-        price = float(row[2])
-        total += nshares * price
+    with open('Data/portfolio.csv', 'rt') as f:
+        rows = csv.reader(row)
+        headers = next(row)
+        total = 0.0
+        for rows in row:
+            try:
+                nshares = int(fields[1])
+                price = float(row[2])
+                total += nshares * price
 
-print('Total', total)
-    
+            except ValueError:
+                print("Bad row", line)
+
+    print('Total', total)
+
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total', cost)   
