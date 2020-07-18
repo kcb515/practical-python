5b6a684ab4a554b827ebd97c55bc6ecb544c52b0
Katie Brown <kcb515@mun.ca>
2020-07-18 15:47:18 -0230

    Exercise 2.16

--- Work/pcost.py
+++ Work/pcost.py
@@ -10,9 +10,10 @@ def portfolio_cost(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for rowno, row in enumerate(rows, start=2):
+            record = dict(zip(headers,row))
             try:
-                nshares = int(row[1])
-                price = float(row[2])
+                nshares = int(record['shares'])
+                price = float(record['price'])
                 total += nshares * price
 
             except ValueError:
