c0377e05673abd5dfdaa78b62b67638de485b378
Katie Brown <kcb515@mun.ca>
2020-07-20 15:06:41 -0230

    Exercise 3.1

--- Work/report.py
+++ Work/report.py
@@ -39,18 +39,23 @@ def make_report_data(portfolio, prices):
     rows = []
     for stock in portfolio:
         current_price = prices[stock['name']]
-        change = current_price - stock['price']
+        change = current_price - stock[['price']]
         summary = (stock['name'], stock['shares'], current_price,change)
         rows.append(summary)
     return rows
 
-portfolio = read_portfolio('Data/portfolio.csv')
-prices = read_prices('Data/prices.csv')
+def print_report(report):
 
-report = make_report_data(portfolio, prices)
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s %10s %10s %10s' % headers)
+    print(('-'* 10 + ' ') * len(headers))
+    for report in report:
+        print('%10s %10d %10.2f %10.2f' % row)
+def portfolio_report(portfolio_filename, prices, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
 
-headers = ('Name', 'Shares', 'Price', 'Change')
-print('%10s %10s %10s %10s' % headers)
-print(('-'* 10 + ' ') * len(headers))
-for row in report:
-    print('%10s %10d %10.2f %10.2f' % row)
+    report = make_report_data(portfolio, prices)
+    print_report(report)
+
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv','Data/prices.csv')
